e0446c5c71d8c8c2d12750ffef0dadcba0376fd8
anso33 <hyeon131812@gmail.com>
2023-11-27 18:42:27 +0900

    :tada: initial project

---
+++ src/main/java/com/anso/oopsisproject/OopSisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.anso.oopsisproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopSisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopSisProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anso/oopsisproject/OopSisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anso.oopsisproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopSisProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
